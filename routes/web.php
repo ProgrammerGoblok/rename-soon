4d37fcba5e30225a0944bcd3e1cdb68d6915a8a6
Afiat yulian <afiat.github@gmail.com>
2022-07-07 20:03:47 +0700

    wpu laravel 15. User Registration

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use App\Models\Post;
-use Illuminate\Support\Facades\Route;
-use App\Models\Category;
 use App\Models\User;
+use App\Models\Category;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -47,6 +49,11 @@ Route::get('/categories', function () {
     ]);
 });
 
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
+
+Route::post('/register', [RegisterController::class, 'store']);
 // Route::get('/categories/{category:slug}', function (Category $category) {
 //     return view('posts', [
 //         'title' => "Post by Category : $category->name",
